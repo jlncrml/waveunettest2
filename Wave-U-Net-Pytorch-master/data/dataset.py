e09d28e6346f4d26819b09232a146e8a5c0a7af9
jlncrml <creepybluehair@gmail.com>
2024-12-20 19:25:24 +0100

    Update dataset.py

--- Wave-U-Net-Pytorch-master/data/dataset.py
+++ Wave-U-Net-Pytorch-master/data/dataset.py
@@ -131,9 +131,6 @@ class SeparationDataset(Dataset):
         piano_source_audio[:self.output_frames_start] = 0
         piano_source_audio[self.output_frames_end:] = 0
 
-        # Combine mix and piano source into separate channels
-        audio = torch.stack((mix_audio, piano_source_audio), dim=0)
-
         # Slice and pad targets
         targets_data = torch.tensor(item["targets"][start_pos:end_pos].astype(np.float32))
         targets_data = F.pad(targets_data.unsqueeze(0), (pad_front, pad_back), 'constant', 0.0).squeeze(0)
