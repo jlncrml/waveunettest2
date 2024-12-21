4211b376cb1d8bd6b43b0ae70e63db03524c2867
jlncrml <creepybluehair@gmail.com>
2024-12-21 19:30:01 +0100

    Update dataset.py

--- Wave-U-Net-Pytorch-master/data/dataset.py
+++ Wave-U-Net-Pytorch-master/data/dataset.py
@@ -8,20 +8,6 @@ import torch.nn.functional as F
 import soundfile as sf
 
 
-def butter_lowpass_filter(data, cutoff_freq=11000, sr=48000, order=6):
-    nyquist = 0.5 * sr
-    b, a = butter(order, cutoff_freq / nyquist, btype='low', analog=False)
-    filtered = lfilter(b, a, data, axis=-1)
-    filtered = filtered.astype(np.float32)
-    return filtered
-
-
-def get_waveform(path):
-    audio, _ = sf.read(path)
-    waveform = np.squeeze(audio).astype("float32")
-    return torch.tensor(waveform, dtype=torch.float32)
-
-
 class SeparationDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -38,44 +24,35 @@ class SeparationDataset(torch.utils.data.Dataset):
         self.output_end = self.output_start + output_length
         self.data = []
 
+        voice_filename = "voice.wav"
+        piano_speaker_bleed_filename = "piano_speaker_bleed.wav"
+        piano_source_filename = "piano_source.wav"
+
         for folder in glob.glob(os.path.join(path, '*')):
-            voice_path = os.path.join(folder, "voice.wav")
-            piano_speaker_bleed_path = os.path.join(folder, "piano_speaker_bleed.wav")
-            piano_source_path = os.path.join(folder, "piano_source.wav")
-            mix_path = os.path.join(folder, "mix.wav")
-
-            voice_waveform = get_waveform(voice_path)
-            piano_speaker_bleed_waveform = get_waveform(piano_speaker_bleed_path)
-            piano_source_waveform = get_waveform(piano_source_path)
-            mix_waveform = get_waveform(mix_path)
-
-            filtered_voice_waveform = butter_lowpass_filter(voice_waveform)
-            filtered_piano_speaker_bleed_waveform = butter_lowpass_filter(piano_speaker_bleed_waveform)
-            filtered_piano_source_waveform = butter_lowpass_filter(piano_source_waveform)
-            filtered_mix_waveform = butter_lowpass_filter(mix_waveform)
-
-            downsampled_voice_waveform = filtered_voice_waveform[::4]
-            downsampled_piano_speaker_bleed_waveform = filtered_piano_speaker_bleed_waveform[::4]
-            downsampled_piano_source_waveform = filtered_piano_source_waveform[::4]
-            downsampled_mix_waveform = filtered_mix_waveform[::4]
-
-            min_length = min(
-                downsampled_voice_waveform.shape[0],
-                downsampled_piano_speaker_bleed_waveform.shape[0],
-                downsampled_piano_source_waveform.shape[0],
-                downsampled_mix_waveform.shape[0],
+            voice_path, piano_speaker_bleed_path, piano_source_path = (
+                os.path.join(folder, filename)
+                for filename in [voice_filename, piano_speaker_bleed_filename, piano_source_filename]
             )
 
-            downsampled_voice_waveform = downsampled_voice_waveform[:min_length]
-            downsampled_piano_speaker_bleed_waveform = downsampled_piano_speaker_bleed_waveform[:min_length]
-            downsampled_piano_source_waveform = downsampled_piano_source_waveform[:min_length]
-            downsampled_mix_waveform = downsampled_mix_waveform[:min_length]
+            voice_waveform, piano_speaker_bleed_waveform, piano_source_waveform = (
+                self.__class__.downsampled_waveform(path) for path in [voice_path, piano_speaker_bleed_path, piano_source_path]
+            )
+
+            min_length = min(
+                voice_waveform.shape[0],
+                piano_speaker_bleed_waveform.shape[0],
+                piano_source_waveform.shape[0],
+            )
+
+            voice_waveform, piano_speaker_bleed_waveform, piano_source_waveform = (
+                waveform[:min_length]
+                for waveform in [voice_waveform, piano_speaker_bleed_waveform, piano_source_waveform]
+            )
 
             self.data.append({
-                "voice_waveform": downsampled_voice_waveform,
-                "piano_speaker_bleed_waveform": downsampled_piano_speaker_bleed_waveform,
-                "piano_source_waveform": downsampled_piano_source_waveform,
-                "mix_waveform": downsampled_mix_waveform,
+                "voice_waveform": voice_waveform,
+                "piano_speaker_bleed_waveform": piano_speaker_bleed_waveform,
+                "piano_source_waveform": piano_source_waveform,
                 "length": min_length
             })
 
@@ -114,8 +91,8 @@ class SeparationDataset(torch.utils.data.Dataset):
         voice_audio = F.pad(voice_audio.unsqueeze(0), (pad_front, pad_back), 'constant', 0.0).squeeze(0)
         piano_bleed_audio = torch.tensor(item["piano_speaker_bleed_waveform"][start_pos:end_pos].astype(np.float32))
         piano_bleed_audio = F.pad(piano_bleed_audio.unsqueeze(0), (pad_front, pad_back), 'constant', 0.0).squeeze(0)
+
         mix_audio = voice_audio + piano_bleed_audio
-        # mix_audio = self.normalize(mix_audio)
         mix_audio[self.output_end:] = 0
 
         piano_source_audio = torch.tensor(item["piano_source_waveform"][start_pos:end_pos].astype(np.float32))
@@ -134,6 +111,25 @@ class SeparationDataset(torch.utils.data.Dataset):
     def __setstate__(self, state):
         self.__dict__.update(state)
 
-    def normalize(self, waveform):
+    @staticmethod
+    def downsampled_waveform(path):
+        return SeparationDataset.lowpass(SeparationDataset.get_waveform(path))[::4]
+
+    @staticmethod
+    def lowpass(waveform, cutoff_freq=11000, sr=48000, order=6):
+        nyquist = 0.5 * sr
+        b, a = butter(order, cutoff_freq / nyquist, btype='low', analog=False)
+        filtered = lfilter(b, a, waveform, axis=-1)
+        filtered = filtered.astype(np.float32)
+        return filtered
+
+    @staticmethod
+    def get_waveform(path):
+        audio, _ = sf.read(path)
+        waveform = np.squeeze(audio).astype("float32")
+        return torch.tensor(waveform, dtype=torch.float32)
+
+    @staticmethod
+    def normalize(waveform):
         peak = torch.max(torch.abs(waveform))
         return waveform / peak if peak > 0 else waveform
